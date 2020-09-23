26fe3b7ad3b7bb7ac9719d88dd5e4a966e40132b
xiongziyuan <mubai.xzy@bitsun-inc.com>
2020-09-23 12:24:11 +0800

    第一个项目架子

---
+++ src/main/java/com/xzy/onlinemall/OnlinemallApplication.java
@@ -0,0 +1,13 @@
+package com.xzy.onlinemall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinemallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlinemallApplication.class, args);
+    }
+
+}
