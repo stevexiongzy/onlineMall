344a7f64d3309ec1107120a01c0f222fd7ac8381
xiongziyuan <mubai.xzy@bitsun-inc.com>
2020-09-23 18:24:19 +0800

    构建商品中心

--- itemcenter/src/main/java/com/mall/itemcenter/ItemcenterApplication.java
+++ itemcenter/src/main/java/com/mall/itemcenter/ItemcenterApplication.java
@@ -1,9 +1,16 @@
 package com.mall.itemcenter;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+/**
+ * @author xiongziyuan
+ */
+@SpringBootApplication(scanBasePackages = {"com.mall.itemcenter"})
+@EnableDiscoveryClient
+@MapperScan("com.mall.itemcenter.dao")
 public class ItemcenterApplication {
 
     public static void main(String[] args) {
