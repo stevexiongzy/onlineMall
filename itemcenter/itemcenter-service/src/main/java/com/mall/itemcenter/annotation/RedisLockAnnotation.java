e0337d466bbbcd128c7b486fd34700de465a90bc
xiongziyuan <mubai.xzy@bitsun-inc.com>
2021-01-05 18:25:13 +0800

    redis锁

---
+++ itemcenter/itemcenter-service/src/main/java/com/mall/itemcenter/annotation/RedisLockAnnotation.java
@@ -0,0 +1,32 @@
+package com.mall.itemcenter.annotation;
+
+import com.mall.itemcenter.common.RedisLockTypeEnum;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author xzy
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD,ElementType.TYPE})
+public @interface RedisLockAnnotation {
+    /**
+     * 特定参数识别，默认取第 0 个下标
+     */
+    int lockFiled() default 0;
+    /**
+     * 超时重试次数
+     */
+    int tryCount() default 3;
+    /**
+     * 自定义加锁类型
+     */
+    RedisLockTypeEnum typeEnum();
+    /**
+     * 释放时间，秒 s 单位
+     */
+    long lockTime() default 30;
+}
