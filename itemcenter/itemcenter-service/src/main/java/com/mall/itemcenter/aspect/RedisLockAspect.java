fb2fc18a2276c8c3940e76643b4c40775c64500e
xiongziyuan <mubai.xzy@bitsun-inc.com>
2021-02-01 16:51:35 +0800

    reids aspect

--- itemcenter/itemcenter-service/src/main/java/com/mall/itemcenter/aspect/RedisLockAspect.java
+++ itemcenter/itemcenter-service/src/main/java/com/mall/itemcenter/aspect/RedisLockAspect.java
@@ -1,7 +1,9 @@
 package com.mall.itemcenter.aspect;
 
 import com.mall.itemcenter.annotation.RedisLockAnnotation;
+import com.mall.itemcenter.common.RedisLockDefinitionHolder;
 import com.mall.itemcenter.common.RedisLockTypeEnum;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -14,13 +16,24 @@ import org.springframework.stereotype.Component;
 import javax.annotation.Resource;
 import java.lang.reflect.Method;
 import java.util.UUID;
+import java.util.concurrent.*;
 
 @Aspect
+@Slf4j
 @Component
 public class RedisLockAspect {
 
     @Resource
     private StringRedisTemplate stringRedisTemplate;
+    // 扫描的任务队列
+    private static ConcurrentLinkedQueue<RedisLockDefinitionHolder> holderList = new ConcurrentLinkedQueue();
+
+    private static final ScheduledExecutorService SCHEDULER = Executors.newSingleThreadScheduledExecutor();
+    {
+        SCHEDULER.scheduleAtFixedRate(() -> {
+
+        },0,2,TimeUnit.SECONDS);
+    }
 
     /**
      * @annotation 中的路径表示拦截特定注解
@@ -42,11 +55,34 @@ public class RedisLockAspect {
         String uniqueValue = UUID.randomUUID().toString();
         // 加锁
         Object result = null;
-        boolean isSuccess = stringRedisTemplate.opsForValue().setIfAbsent(businessKey,uniqueValue);
+
+        long lockTime = annotation.lockTime();
+        boolean isSuccess = stringRedisTemplate.opsForValue().setIfAbsent(businessKey,uniqueValue,lockTime, TimeUnit.SECONDS);
         if(!isSuccess){
             throw new Exception("another get lock");
         }
 
-        return null;
+        result = pjp.proceed();
+        //开启一个线程检查超时状态
+        new Thread(new UpdateLockTimeoutTask(uniqueValue,businessKey,stringRedisTemplate));
+
+        return result;
+    }
+
+    class UpdateLockTimeoutTask implements Runnable {
+        private String uuid;
+        private String key;
+        private StringRedisTemplate stringRedisTemplate;
+
+        public UpdateLockTimeoutTask(String uuid, String key, StringRedisTemplate stringRedisTemplate) {
+            this.uuid = uuid;
+            this.key = key;
+            this.stringRedisTemplate = stringRedisTemplate;
+        }
+
+        @Override
+        public void run() {
+
+        }
     }
 }
